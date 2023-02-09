df1b53d1daac21acffb7e6ac2fae4b5ca1545fb6
benstah <koehler.benedikt@gmx.de>
2023-02-09 14:45:36 +0100

    stats file adjusted for reading the right files without file ending

--- data/print_stats.py
+++ data/print_stats.py
@@ -5,27 +5,27 @@ import os
 dirname = os.path.dirname(__file__)
 
 # shows stats of all data sets
-df_training_s = load(dirname + '/training_set_s.pkl')
+df_training_s = load(dirname + '/training_set_s')
 
 print("----------------- Training Set Small -----------------")
 print("Rows: " + str(len(df_training_s.index)))
 print(df_training_s.head())
  
-df_training_l = load(dirname + '/training_set_l.pkl')
+df_training_l = load(dirname + '/training_set_l')
 
 print("----------------- Training Set Large -----------------")
 print("Rows: " + str(len(df_training_l.index)))
 print(df_training_l.head())
 
 
-df_gold_validation = load(dirname + '/validation_set.pkl')
+df_gold_validation = load(dirname + '/validation_set')
 
 print("----------------- Gold Validation Set -----------------")
 print("Rows: " + str(len(df_gold_validation.index)))
 print(df_gold_validation.head())
 
 
-df_test = load(dirname + '/test_set.pkl')
+df_test = load(dirname + '/test_set')
 
 print("----------------- Test Set -----------------")
 print("Rows: " + str(len(df_test.index)))
