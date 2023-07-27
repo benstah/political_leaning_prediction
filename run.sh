4b8fad635627c39efade53766a3ea18c52c138df
benstah <koehler.benedikt@gmx.de>
2023-07-27 23:00:43 +0200

    batch file to start the job on the server

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --nodes=1
+#SBATCH --job-name=political_leaning
+#SBATCH --output=political_leaning%j.out
+#SBATCH --cpus-per-task 2 # number of cpus
+#SBATCH --mem 64g # memory pool for all cores
+#SBATCH -o myoutput.out # STDOUT
+#SBATCH -e myerroutput.out # STDERR
+#SBATCH --gres=gpu:1 # number of gpus
+
+source /storage/sedovaa20/benedikt/political_leaning_venv
+cd /storage/sedovaa20/benedikt/political_leaning_prediction
+pip3 install requirements.txt
+python3 src/models/k_fold_model.py
